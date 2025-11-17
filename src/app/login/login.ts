9d59ef029ae890efd35c3af06ebb3f127c761bb9
rafaatrevisan <rafa_tre2@hotmail.com>
2025-11-16 23:54:35 -0300

    Alterações no home e modal de esqueci minha senha

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router'; 
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -39,5 +40,33 @@ export class LoginComponent {
       this.errorMessage = 'Email ou senha inválidos';  
     }
   );
+}
+
+abrirRecuperacaoSenha(event: Event) {
+  event.preventDefault(); 
+
+  Swal.fire({
+    title: 'Recuperar senha',
+    text: 'Informe o email associado à sua conta:',
+    input: 'email',
+    inputPlaceholder: 'seuemail@exemplo.com',
+    showCancelButton: true,
+    confirmButtonText: 'Enviar',
+    cancelButtonText: 'Cancelar',
+    confirmButtonColor: '#0884c0',
+    customClass: {
+      popup: 'custom-swal',
+      
+    }
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Swal.fire({
+        icon: 'success',
+        title: 'Email enviado!',
+        text: `Se existir uma conta associada ao email ${result.value}, você receberá instruções para redefinir sua senha.`,
+        confirmButtonText: 'OK'
+      });
+    }
+  });
 }
 }
